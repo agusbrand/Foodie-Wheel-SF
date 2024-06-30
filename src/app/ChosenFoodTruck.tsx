1f50b3eb5143f5c58ed54b0cc59e8303758dc8c8
Agustin Brandoni <agusbrand@gmail.com>
2024-06-30 11:42:48 -0300

    Add Chosen Food Truck component

--- src/app/ChosenFoodTruck.tsx
+++ src/app/ChosenFoodTruck.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import React from "react";
 import { useFoodTruckContext } from "../context/FoodTruckContext";
-import { Box, Typography } from "@mui/material";
+import {
+  Stack,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import { green } from "@mui/material/colors";
+import GoogleMapsLink from "@/components/GoogleMapsLink";
 
-const ChosenFoodTruck = () => {
+const ChosenFoodTruck: React.FC = () => {
   const { chosenFoodTrucks } = useFoodTruckContext();
 
   if (chosenFoodTrucks.length === 0) {
@@ -14,23 +23,53 @@ const ChosenFoodTruck = () => {
   const foodTruckDescription = chosenFoodTrucks[0].description;
 
   return (
-    <Box>
-      <Typography variant="h2">{foodTruckName}</Typography>
-      <Typography>{foodTruckDescription}</Typography>
+    <>
+      <Stack spacing={3} marginTop={12} textAlign="center">
+        <Typography
+          variant="h3"
+          color={green[700]}
+          fontWeight={600}
+          fontSize={"5rem"}
+          aria-label={`Food Truck Name: ${foodTruckName}`}
+        >
+          {foodTruckName}
+        </Typography>
+        <Typography
+          fontSize={"2rem"}
+          aria-label={`Food Truck Description: ${foodTruckDescription}`}
+        >
+          {foodTruckDescription}
+        </Typography>
+      </Stack>
 
-      {chosenFoodTrucks.map((foodTruck, index) => (
-        <Box key={index} borderBottom="solid 2px black">
-          <Typography key={index} variant="h4">
-            {foodTruck.name}
-          </Typography>
-          <Typography>{foodTruck.type}</Typography>
-          <Typography>{foodTruck.address}</Typography>
-          <Typography>{foodTruck.addressDescription}</Typography>
-          <Typography>{foodTruck.latitude}</Typography>
-          <Typography>{foodTruck.longitude}</Typography>
-        </Box>
-      ))}
-    </Box>
+      <Stack spacing={5} marginTop={6}>
+        <Typography fontWeight={600} fontSize={21}>
+          Food Truck Locations
+        </Typography>
+
+        {chosenFoodTrucks.map((foodTruck) => (
+          <Card key={foodTruck.id} variant="outlined" sx={{ marginBottom: 3 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {foodTruck.type}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {foodTruck.address}
+              </Typography>
+              <Typography variant="body2">
+                {foodTruck.addressDescription}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <GoogleMapsLink
+                latitude={foodTruck.latitude}
+                longitude={foodTruck.longitude}
+              />
+            </CardActions>
+          </Card>
+        ))}
+      </Stack>
+    </>
   );
 };
 
